6133b4d1dc1de5e06633e0c7342c8ae1e5140a20
Gianluca Forgia <gianluca.forgia@gmail.com>
2021-04-08 15:19:45 +0200

    ciclo l'array di albums e con il ciclo mi ricavo i generi

--- js/script.js
+++ js/script.js
@@ -2,6 +2,7 @@ var root = new Vue({
 el: '#root',
  data: {
    albums: [],
+   genres:[],
 
 
     },
@@ -9,11 +10,26 @@ el: '#root',
      axios.get('https://flynn.boolean.careers/exercises/api/array/music')
      .then( (resp) => {
         this.albums =  resp.data.response;
+        // Bonus 1
+        //Ciclo l'array albums per prendere i generi
+        this.albums.forEach(( album ) => {
+          if (!this.genres.includes(album.genre) ) {
+                this.genres.includes(album.genre);
+                 console.log(album.genre);
+                 console.log(this.genres);
+                 }
+
+        });
+
+
      });
+
+
 
    },
 
    methods: {
+
 
    }
 
